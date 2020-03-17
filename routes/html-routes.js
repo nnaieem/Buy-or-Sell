62c3b1097c69a05625f013fca3a944d4a8c48087
nnaieem <nicholas.naieem@gmail.com>
2020-03-17 18:03:53 -0400

    change connection

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -19,13 +19,28 @@ var mysql = require("mysql")
 var PORT = process.env.PORT || 8080;
 
 // MySQL DB Connection Information (remember to change this with our specific credentials)
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "files"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "files"
+// });
+
+var connection;
+if (process.env.JAWSDB_URL) {
+  // Database is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // Database is local
+  connection = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'files'
+  })
+};
 
 // Initiate MySQL Connection.
 connection.connect(function(err) {
