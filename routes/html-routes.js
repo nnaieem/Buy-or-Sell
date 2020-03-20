4277700a933d107c3d0c78da24a84c7584279c2c
nnaieem <nicholas.naieem@gmail.com>
2020-03-19 22:06:20 -0400

    test port remove

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -16,7 +16,7 @@ var mysql = require("mysql")
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
